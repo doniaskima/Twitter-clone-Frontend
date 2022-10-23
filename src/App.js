686495fe5e16d9366ebb66b193b2a912152070f1
unknown <doniaskima344@gmail.com>
2022-10-23 11:07:29 +0100

    App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
+import {Router} from "react-router-dom";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
 
 function App() {
   return (
-    <div className="App">
-
+    <div className="App h-screen">
+    <Router>
+        <Login path="/" />
+        <Signup path="signup"/>
+    </Router>
     </div>
   );
 }
