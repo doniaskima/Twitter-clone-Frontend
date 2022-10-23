de7affa8522765b7726db82f1837ea5872067143
unknown <doniaskima344@gmail.com>
2022-10-23 12:33:51 +0100

    working on login pasg

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import {useState} from 'react'
+import { Link, useNavigate } from "react-router-dom";
+import {useAuth} from "../Context/authProvider"
 
-const login = () => {
+const Login = () => {
+  const { loginUserWithCredentials, validateEmail } = useAuth();
+  const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const loginHandler = async (event) => {
+    event.preventDefault();
+    setError("");
+    setLoading(true);
+  }
+
   return (
-   <h1>Helllo Login</h1>
+    <div className="h-screen flex justify-center">
+      <div className="text-center mt-40">
+        <h1 className="text-5xl font-semibold mb-4">Twitter Clone</h1>
+        <p className="text-red-50 font-medium">{error}</p>
+        <div className="text-left shadow-lg w-96 p-4 bg-gray-200 rounded-md mt-2 ">
+          <form
+            onSubmit={(e) => {
+              loginHandler(e);
+            }}
+          >
+            <div className="">
+              <label htmlFor="email" className="font-semibold">Email</label>
+              <input ocChange={(e)=>{setEmail(e.target.value)}} placeholdre="Your password" id="password" value={password} className="bg-transparent rounded-md w-11/12 p-2 flex items-center"/>
+            </div>
+
+          </form>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
-export default login
+export default Login
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
