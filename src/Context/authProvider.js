3cc0b162840a95901fd6656543d00add10069270
unknown <doniaskima344@gmail.com>
2022-10-23 11:20:25 +0100

    signupUserWithCredentials

--- src/Context/authProvider.js
+++ src/Context/authProvider.js
@@ -24,6 +24,22 @@ const AuthProvider = ({ children }) => {
             console.log(error.message);
         }
     };
+
+    const signupUserWithCredentials = async (username, name, email, password) => {
+      try {
+        const { data } = await axios.post(`${BaseUrl}/users/signup`, { username, name, email, password, });
+        if (data.success) {
+          setUser(data.user);
+          setToken(data.token);
+          axios.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
+          localStorage.setItem("user", JSON.stringify(data.user));
+          localStorage.setItem("token", JSON.stringify(data.token));
+        }
+        return { user: data.user, message: data.message, success: data.success };
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
     return (
     <AuthContext.Provider value={{ user, token, loginUserWithCredentials }}>
       {children}
