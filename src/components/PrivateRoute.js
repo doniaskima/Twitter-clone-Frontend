b933dcadcb18aa13a1d055bb5758f9a7a8c67576
unknown <doniaskima344@gmail.com>
2022-10-27 05:15:00 +0100

    PrivateRouter added

---
+++ src/components/PrivateRoute.js
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom";
+import useAuth from "../Context/authProvider";
+
+const PrivateRouter = ({ component: Component, path }) => {
+    const { user } = useAuth();
+    return user == null ? (
+        <Navigate to="/"/>
+    ) : (
+           <Component path="/"/> 
+    )
+}
+
+export default PrivateRouter;
