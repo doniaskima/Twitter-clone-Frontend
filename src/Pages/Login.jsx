033bab269c033b9bc1013d59a7f8cacce20676a1
unknown <doniaskima344@gmail.com>
2022-10-27 05:25:34 +0100

    update Login Page

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -2,6 +2,7 @@ import {useState} from 'react'
 import {  useNavigate } from "react-router-dom";
 import {useAuth} from "../Context/authProvider";
 import { Link } from "react-router-dom";
+import { Input, Label } from "../components/FormComponents";
 
 const Login = () => {
   const { loginUserWithCredentials, validateEmail } = useAuth();
@@ -10,6 +11,7 @@ const Login = () => {
   const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+ 
 
   const loginHandler = async (event) => {
     event.preventDefault();
@@ -36,21 +38,28 @@ const Login = () => {
   return (
     <div className="h-screen flex justify-center">
       <div className="text-center mt-40">
-        <h1 className="text-5xl font-semibold mb-4">Twitter Clone</h1>
-        <p className="text-red-50 font-medium">{error}</p>
-        <div className="text-left shadow-lg w-96 p-4 bg-gray-200 rounded-md mt-2 ">
-          <form onSubmit={(e) => {
-            loginHandler(e);
-          }}>
+        <h1 className="text-5xl font-semibold mb-4">Twitter clone</h1>
+        <p className="text-red-600 font-medium">{error}</p>
+        <div className="text-left shadow-lg w-96 p-4 bg-gray-200 rounded-md mt-2">
+          <form onSubmit={loginHandler}>
             <div className="">
-              <label htmlFor="email" className="font-semibold">Email</label>
-              <input onChange={(e) => setEmail(e.target.value)} type="text" placeholder="Your Email" id="email" className="border-2 border-gray-400 rounded-md w-full p-2 my-2" />
-              
+              <Label htmlFor="email" labelText="Email" id="email-input-title" />
+              <Input
+                callback={setEmail}
+                placeholder="Your Email"
+                value={email}
+                id="email"
+                ariaLabelledBy="email-input-title"
+              />
             </div>
             <div>
-              <label htmlFor="password" className="font-semibold">Password</label>
-              <div className="border-2 border-gray-400 bg-white rounded-md flex-items-centered">
-              <input
+              <Label
+                htmlFor="password"
+                labelText="Password"
+                id="password-input-title"
+              />
+              <div className="border-2 border-gray-400 bg-white rounded-md flex items-center">
+                <input
                   onChange={(e) => {
                     setPassword(e.target.value);
                   }}
@@ -59,42 +68,40 @@ const Login = () => {
                   value={password}
                   type={showPassword ? "text" : "password"}
                   className="bg-transparent rounded-md w-11/12 p-2 focus:outline-none"
+                  aria-labelledby="password-input-title"
                 />
-                {
-                  showPassword ? (
-                    <i
+                {showPassword ? (
+                  <i
                     className="fa fa-eye-slash cursor-pointer"
                     onClick={() => setShowPassword(false)}
                   ></i>
-                  ) : (
-                    <i
+                ) : (
+                  <i
                     className="fa fa-eye cursor-pointer"
                     onClick={() => setShowPassword(true)}
                   ></i>
-                  )
-                }
-             </div>
+                )}
+              </div>
             </div>
-            <div className="flex justify-center md-4">
+            <div className="flex justify-center mt-2">
               <button
                 type="submit"
                 disabled={email === "" || password === ""}
                 className="bg-blue-500 text-white px-3 py-2 rounded-md my-2 w-36 font-semibold"
               >
-                {!loading ? "Login" : "Logging In.."}
-             </button>
+                {!loading ? "Login" : "Logging In..."}
+              </button>
             </div>
           </form>
           <div className="text-center mt-4">
-            <p className="font-semibold">
-                 Dont have an account ?   
+            <p className="font-normal">
+              Dont have an account? 
             </p>
           </div>
         </div>
-        
       </div>
     </div>
-  )
+  );
 }
 
 export default Login
