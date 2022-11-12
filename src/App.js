c665149fe060ca7eafb043e3e9727726eca758ef
unknown <doniaskima344@gmail.com>
2022-11-13 00:17:49 +0100

    fixex

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {BrowserRouter as Router , Route, Routes } from "react-router-dom";
+import {BrowserRouter as Router,Route, Routes } from "react-router-dom";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -3,8 +3,6 @@ import {Input , Label} from "../components/FormComponents";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
  
-
-
 const Signup = () => {
   const navigate = useNavigate();
   const { signupUserWithCredentials, validateEmail } = useAuth();
