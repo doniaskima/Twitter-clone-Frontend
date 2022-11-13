a3fb3af7ad6a51bed99e0cf9e4aec933c302f46e
unknown <doniaskima344@gmail.com>
2022-11-13 03:06:48 +0100

    gonna add it

---
+++ src/features/notification/notificationsSlice.js
@@ -0,0 +1,11 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { BaseUrl } from "../../utils/BaseUrl";
+
+const notificationsSlice = () => {
+  return (
+    <div>notificationsSlice</div>
+  )
+}
+
+export default notificationsSlice
