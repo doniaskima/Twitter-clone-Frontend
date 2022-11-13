ac438e01e393ab08faa25a7bd1267f4a029883cc
unknown <doniaskima344@gmail.com>
2022-11-13 04:03:21 +0100

    fetchNotifications added

--- src/features/notification/notificationsSlice.js
+++ src/features/notification/notificationsSlice.js
@@ -4,5 +4,22 @@ import { BaseUrl } from "../../utils/BaseUrl";
 
 
 export const fetchNotifications = createAsyncThunk(
-    
+
+    "notification/fetchNotifications",
+    async ({ userId, thunkAPI }) => {
+        try {
+            const { data } = await axios.get(`${BaseUrl}/users/notifications/${userId}`);
+            if (data.success) {
+                return data;
+            }
+            return thunkAPI.rejectWithValue({
+                errorMessage: data.message
+            });
+        } catch (error) {
+            return thunkAPI.rejectWithValue({
+                errorMessage: error.message
+            });
+        }
+    }
+
 )
