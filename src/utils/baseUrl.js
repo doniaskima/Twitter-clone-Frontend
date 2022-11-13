6026bf21d67deeff0f861a5b8c7cd109b13b2e5c
unknown <doniaskima344@gmail.com>
2022-11-13 03:03:26 +0100

    update utils

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,1 +1,6 @@
 export const BaseUrl = "https://donia-twitter-clone.herokuapp.com";
+export function validateEmail(email) {
+    return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
+        email
+    );
+}
