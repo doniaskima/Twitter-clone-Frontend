75c27086d391501722d52f47bc2b34b478005234
unknown <doniaskima344@gmail.com>
2022-11-14 03:36:00 +0100

    still working on userSlice

---
+++ src/features/user/userSlice.js
@@ -0,0 +1,27 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+import { BaseUrl } from "../../utils/BaseUrl";
+
+
+
+export const loginUserAsync = createAsyncThunk(
+    "user/login",
+    async ({ email, password }, thunkAPI) => {
+        try {
+            const { data } = await axios.post(`{BaseUrl}/users/login`, { email, password, });
+            if (data.success) {
+                axios.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
+                localStorage.setItem("user", JSON.stringify(data.user));
+                localStorage.setItem("token", JSON.stringify(data.token));
+                return data;
+            }
+            return thunkAPI.rejectWithValue({
+                errorMessage: data.message
+            })
+        } catch (error) {
+            return thunkAPI.rejectWithError({
+                errorMessage: error.message
+            });
+        }
+    }
+);
