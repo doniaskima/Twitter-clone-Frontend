e8bf9ffbf17c6b444bc5b18fbf2fe5baf0919e42
unknown <doniaskima344@gmail.com>
2022-11-14 03:07:05 +0100

    Spinner component added

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -1,13 +1,13 @@
-// import Spinner from "./Spinner";
+import Spinner from "./Spinner";
 
-// const LoadingPage = () => {
-//     return (
-//         <div className="flex h-screen h-full w-full flex-col justify-center bg-white bg-blend-saturation">
-//             <Spinner />
-//             <h1 className="mt-4 text-lg font-semibold tracking-wider">Loading...</h1>
-//         </div>
-//     )
-// }
+const LoadingPage = () => {
+    return (
+        <div className="flex h-screen h-full w-full flex-col justify-center bg-white bg-blend-saturation">
+            <Spinner />
+            <h1 className="mt-4 text-lg font-semibold tracking-wider">Loading...</h1>
+        </div>
+    )
+}
 
 
-// export default LoadingPage;
+export default LoadingPage;
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -2,5 +2,7 @@ import { ImSpinner9 } from "react-icons/im";
 
 
 const Spinner = () => {
-    return <ImSpinner9/>
+    return <ImSpinner9 className="text-5xl animate-spin text-blue-500"/>
 }
+
+export default Spinner;
