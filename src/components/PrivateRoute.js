3a1f19e1abd540c7cab3a896fa92dabdb544308c
unknown <doniaskima344@gmail.com>
2022-11-14 02:47:05 +0100

    chore:re-implemented private route

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -1,13 +1,19 @@
-import { Navigate } from "react-router-dom";
-import { useAuth } from "../Context/authProvider";
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+import LoadingPage from "./LoadingPage";
 
-const PrivateRouter = ({ component: Component, path }) => {
-    const { user } = useAuth();
-    return user == null ? (
-        <Navigate to="/"/>
-    ) : (
-            <Component path={path} /> 
-    )
+const PrivateRouter = () => {
+    
+    const userState = useSelector((state) => state.user);
+    const initialLoading = useSelector((state) => state.initialLoading);
+    if (initialLoading) {
+        return <LoadingPage />;
+    }
+    if (userState.isUserLoggedIn) {
+        return <Outlet />;
+    }
+    
+    return <Navigate to="/" replace/>
 }
 
 export default PrivateRouter;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import "./index.css";
 import App from "./App";
 import { AuthProvider } from "./Context/authProvider";
 import store from "./Store/store";
-import {BrowserRouter} from "react-router-dom"
+import { Provider } from "react-redux";
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+  <Provider store={store}>
+    <App />
+  </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
