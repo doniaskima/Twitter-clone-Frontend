4f4474ae4c170659521411c6e6f4d42dc462196e
unknown <doniaskima344@gmail.com>
2022-11-14 21:08:29 +0100

    come back

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -8,7 +8,7 @@ export const loginUserAsync = createAsyncThunk(
     "user/login",
     async ({ email, password }, thunkAPI) => {
         try {
-            const { data } = await axios.post(`{BaseUrl}/users/login`, { email, password, });
+            const { data } = await axios.post(`${BaseUrl}/users/login`, { email, password, });
             if (data.success) {
                 axios.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
                 localStorage.setItem("user", JSON.stringify(data.user));
@@ -25,6 +25,29 @@ export const loginUserAsync = createAsyncThunk(
         }
     }
 );
+
+
+export const signupUserAsync = createAsyncThunk(
+    "user/signup",
+    async ({ username, name, email, password }, thunkAPI) => {
+        try {
+            const { data } = await axios.post(`${BaseUrl}/users/signup`, {
+                username, name, email, password,
+            });
+            if (data.success) {
+                axios.defaults.headers.common["Authorization"] = `Bearer ${data.token}`;
+                localStorage.setItem("user", JSON.stringify(data.user));
+                localStorage.setItem("token", JSON.stringify(data.token));
+                return data;
+            }
+            return thunkAPI.rejectWithValue({
+                errorMessage: data.message
+            })
+        } catch (error) {
+            return thunkAPI.rejectWithValue({ errorMessage: error.message });
+        }
+    }
+)
 
 
 export default userSlice.reducer;
