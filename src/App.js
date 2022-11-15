fb216bfa4a0cc3be348b8d4eff5ffebbb52b0dda
unknown <doniaskima344@gmail.com>
2022-11-16 00:34:18 +0100

    add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import { useEffect } from "react";
 import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Login from "./Pages/Login";
-import Signup from "./Pages/Signup";
-import Home from "./Pages/Home";
+import {
+  Home,
+  Login,
+  Notification,
+  Signup,
+  Profile,
+  Messages,
+  PostInfo,
+} from "./Pages/index";
 import PrivateRoute from "./components/PrivateRoute";
 import { useDispatch } from "react-redux";
 import { setUserFromLocalStorage } from "./features/user/userSlice";
@@ -21,6 +27,16 @@ function App() {
         <Route path="/signup" element={<Signup />} />
         <Route path="/home" element={<PrivateRoute />}>
           <Route path="/home" element={<Home/>}/>
+        </Route>
+        <Route path="/notifications" element={<PrivateRoute />}>
+          <Route path="/notifications" element={<Notification />} />
+        </Route>
+        <Route path="/profile/:userId" element={<PrivateRoute />}>
+          <Route path="/profile/:userId" element={<Profile />}  
+        <Route />
+          <Route path="/messages" element={<PrivateRoute />>
+            <Route path="/messages" element={<Messages />}>
+          </Route>
         </Route>
       </Routes>
     </div>
