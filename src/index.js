6ab17024d2e27cca4cdf5ecb0d1246501894b3d6
unknown <doniaskima344@gmail.com>
2022-11-16 00:23:26 +0100

    feat: dayjs relativeTime plugin CDN

--- src/index.js
+++ src/index.js
@@ -4,15 +4,13 @@ import "./index.css";
 import App from "./App";
 import store from "./Store/store";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
+  <React.StrictMode> 
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
+
 );
