2502f00b94184bdca503cf06c2bdbb1566577339
unknown <doniaskima344@gmail.com>
2022-11-15 23:58:10 +0100

    feat: NavLink custom reusable component

---
+++ src/components/SideNavigationBar/SideBarLink.js/SideBarNavLink.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { NavLink } from "react-router-dom";
+
+const SideBarNavLink = ({children,ariaLabel,role,to}) => {
+  return (
+    <NavLink
+      to={to}
+      aria-label={ariaLabel}
+      role={role}
+      className={({ isActive }) =>
+        `flex items-center mt-1 space-x-2 ${
+          isActive ? "nav-element-active" : "nav-element"
+        }`
+      }
+    >
+             {children} 
+    </NavLink>
+  )
+}
+
+export default SideBarNavLink
