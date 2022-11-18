a204eea3612aa694dddbe9a380f4756bb32f179b
unknown <doniaskima344@gmail.com>
2022-11-18 04:47:29 +0100

    Feed component added

--- src/components/HomePageComponents/Feed.js
+++ src/components/HomePageComponents/Feed.js
@@ -1,10 +1,35 @@
 import { useSelector } from "react-redux";
 import Spinner from "../Spinner";
+import   Post   from "./Post";
 
 
 const Feed = () => {
+  const { feed, loading, errMessage } = useSelector((state) => state.post);
+  
   return (
-    <div>Feed</div>
+    <div>
+      {loading ? (
+        <div className="flex justify-center mt-4">
+          <Spinner/>
+        </div>
+      ): (
+          feed.map((post) => {
+            return <Post />
+          })
+      )}
+      {
+        !feed && errMessage && (
+          <div className="text-center font-medium mt-4">{errMessage}</div>
+        )
+      }
+      {
+        !loading && feed.length === 0 && (
+          <div className="text-center font-medium mt-4">
+            No posts to display
+          </div>
+        )
+      }
+    </div>
   )
 }
 
