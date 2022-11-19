77db24675b8c240482e519d6d3e8e629954b95f6
unknown <doniaskima344@gmail.com>
2022-11-19 19:48:22 +0100

    ah w be

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,4 +1,4 @@
-export const BaseUrl = "https://nova-socials.herokuapp.com";
+export const BaseUrl = "https://donia-twitter-clone.herokuapp.com";
 export function validateEmail(email) {
     return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
         email
