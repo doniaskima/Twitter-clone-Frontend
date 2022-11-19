49d045c2622292adf4f558884d4fc42db49c0822
unknown <doniaskima344@gmail.com>
2022-11-19 19:47:53 +0100

    ahh wbr

--- src/components/PostInfoPageComponents/Comment.jsx
+++ src/components/PostInfoPageComponents/Comment.jsx
@@ -4,26 +4,38 @@ import { deleteComment } from "../../features/post/postSlice";
 import dayjs from "dayjs";
 import { useDispatch, useSelector } from "react-redux";
 
-const Comment = () => {
-    const { comments, commentsLoading, errMessage } = useSelector((state) => state.user.data._id);
-    const userId = useSelector((state) => state.user.data._id);
-    const dispatch = useDispatch();
+export const Comment = ({ comment }) => {
+  const userId = useSelector((state) => state.user.data._id);
+  const isCommentAuthor = comment.commentBy === userId;
+  const dispatch = useDispatch();
   return (
-      <div>
-          {errMessage && <div>{errMessage}</div>}
-          {commentsLoading ? (
-        <div className="flex justify-center mt-2">
-          <Spinner />
+    <div className="border p-2 flex justify-between">
+      <div className="w-10 h-10 rounded-full bg-gray-200"></div>
+      <div className="ml-2 w-full">
+        <div>
+          <div>
+            <span className="font-semibold">{comment?.commenterName}</span>
+            <div className="float-right text-gray-400">
+              <span className=" text-sm ">{`${dayjs(
+                comment.createdAt
+              ).fromNow()}`}</span>
+              {isCommentAuthor && (
+                <button
+                  onClick={() =>
+                    dispatch(deleteComment({ commentId: comment._id }))
+                  }
+                  className="small-button"
+                >
+                  Delete
+                </button>
+              )}
+            </div>
+          </div>
+          <p className="text-gray-400">{`@${comment?.commenterUsername}`}</p>
+          <div className="my-2">{comment.comment}</div>
         </div>
-          ): (
-                  comments.map((comment) => {
-                      return (
-                      //add code Here Donia
-                  )
-              })    
-          )}
-  </div>
-  )
-}
-
-export default Comment
+        <div></div>
+      </div>
+    </div>
+  );
+};
