b1847a211add1b5b1c0dfa965ca756a8823075d9
unknown <doniaskima344@gmail.com>
2022-11-26 18:33:20 +0100

    UserPosts component added

--- src/components/ProfilePageComponents/UserPosts.jsx
+++ src/components/ProfilePageComponents/UserPosts.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchUserPosts } from "../../features/post/postSlice";
+import Feed from "../HomePageComponents/Feed";
 
-const UserPosts = () => {
-  return (
-    <div>UserPosts</div>
-  )
+export const UserPosts = ({userId}) => {
+  const { userPosts } = useSelector((state) => state.post);
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(fetchUserPosts({ userId }));
+  }, []);
+  
+
+    return <Feed feed={userPosts}/>
+ 
 }
 
-export default UserPosts
