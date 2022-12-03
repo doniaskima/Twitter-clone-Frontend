3ad608731e3c5050832c5040e8b5fc59ca307bec
unknown <doniaskima344@gmail.com>
2022-12-03 13:11:37 +0100

    Bye heroku

--- src/utils/baseUrl.js
+++ src/utils/baseUrl.js
@@ -1,4 +1,4 @@
-export const BaseUrl = "https://donia-twitter-clonee.herokuapp.com";
+export const BaseUrl = "https://twitter-clone-backend.onrender.com";
 
 export function validateEmail(email) {
     return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
