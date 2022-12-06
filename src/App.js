a2cf8cfe0c5ee986b00bbdeb87b3cd5bd1afac18
unknown <doniaskima344@gmail.com>
2022-12-06 20:43:11 +0100

    passing socket context provider

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import Profile from "./Pages/Profile";
 import PrivateRoute from "./components/PrivateRoute";
 import { useDispatch } from "react-redux";
 import { setUserFromLocalStorage } from "./features/user/userSlice";
-
+import { socket, SocketContext } from "./SocketContext/socketContext";
 
 function App() {
   const dispatch = useDispatch();
@@ -29,31 +29,32 @@ function App() {
   }, []);
 
   return (
-    <div className="App h-screen">
-       
-      <Routes>   
+    <SocketContext.Provider value={socket}>
+      <div className="App h-screen">
+        <Routes>
           <Route path="/" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/home" element={<PrivateRoute />}>
-          <Route path="/home" element={<Home />} />
-        </Route>
-        <Route path="/notifications" element={<PrivateRoute />}>
-          <Route path="/notifications" element={<Notification />} />
-        </Route>
-        <Route path="/profile/:userId" element={<PrivateRoute />}>
-          <Route path="/profile/:userId" element={<Profile />} />
-        </Route>
-        <Route path="/explore" element={<PrivateRoute />}>
-          <Route path="/explore" element={<Explore />} />
-        </Route>
-        <Route path="/messages" element={<PrivateRoute />}>
-          <Route path="/messages" element={<Messages />} />
-        </Route>
-        <Route path="/post/:postId" element={<PrivateRoute />}>
-          <Route path="/post/:postId" element={<PostInfo />} />
-        </Route>   
-      </Routes>
-    </div>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<PrivateRoute />}>
+            <Route path="/home" element={<Home />} />
+          </Route>
+          <Route path="/explore" element={<PrivateRoute />}>
+            <Route path="/explore" element={<Explore />} />
+          </Route>
+          <Route path="/notifications" element={<PrivateRoute />}>
+            <Route path="/notifications" element={<Notification />} />
+          </Route>
+          <Route path="/profile/:userId" element={<PrivateRoute />}>
+            <Route path="/profile/:userId" element={<Profile />} />
+          </Route>
+          <Route path="/messages" element={<PrivateRoute />}>
+            <Route path="/messages" element={<Messages />} />
+          </Route>
+          <Route path="/post/:postId" element={<PrivateRoute />}>
+            <Route path="/post/:postId" element={<PostInfo />} />
+          </Route>
+        </Routes>
+      </div>
+    </SocketContext.Provider>
   );
 
 }
