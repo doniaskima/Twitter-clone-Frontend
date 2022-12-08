7ad95b43504b051f6b33b11837cdc9b6a5e096c0
unknown <doniaskima344@gmail.com>
2022-12-08 15:00:22 +0100

    something wrong

--- src/components/MessagesPageComponents/chats.js
+++ src/components/MessagesPageComponents/chats.js
@@ -1,6 +1,8 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
+import { fetchMessages, newMessage } from "../../features/message/messageSlice";
+import { useSocket } from "../../SocketContext/socketContext";
 
 const chats = () => {
   return (
