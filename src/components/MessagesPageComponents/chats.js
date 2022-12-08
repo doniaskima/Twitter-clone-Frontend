0b2580729e6b50a2f2d274b71b79a30254ee5099
unknown <doniaskima344@gmail.com>
2022-12-08 14:56:40 +0100

    huge problem

---
+++ src/components/MessagesPageComponents/chats.js
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
+
+const chats = () => {
+  return (
+    <div>chats</div>
+  )
+}
+
+export default chats
