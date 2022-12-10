66366d2d8ae6542e617fd5db89d08b4a44c49c60
unknown <doniaskima344@gmail.com>
2022-12-10 23:27:57 +0100

    changes

--- src/Pages/Messages.js
+++ src/Pages/Messages.js
@@ -1,8 +1,8 @@
 import React from 'react'
-
+import Header from "../components/MessagesPageComponents/Header"
 const Messages = () => {
   return (
-    <div>Messages</div>
+   <Header/>
   )
 }
 
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,29 +1,28 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-import { validateEmail } from "../utils/BaseUrl";
+import { useState } from "react";
 import { Input, Label } from "../components/FormComponents";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import { signupUserAsync } from "../features/user/userSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { validateEmail } from "../utils/BaseUrl";
+import { useNavigate } from "react-router-dom";
 
-
-const Signup = () => {
-   
-  const [name,setName]=useState("");
+export const Signup = () => {
+  const [name, setName] = useState("");
   const [username, setUsername] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [error, setError] = useState("");
   const [showPassword, setShowPassword] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const [confirmPassword, setConfirmPassword] = useState("");
   const { loading, errorMessage } = useSelector((state) => state.user);
   const matchPassword = confirmPassword === password;
 
   const isPasswordValid = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,32}$/.test(
     password
   );
+
   const signupHandler = async (event) => {
     event.preventDefault();
     setError("");
@@ -41,7 +40,6 @@ const Signup = () => {
       setError("Please enter a valid email");
       return;
     }
-
     if (!isPasswordValid) {
       setError(
         "Password must be 8 characters long, have one upper and lower case character and one number."
@@ -63,32 +61,31 @@ const Signup = () => {
     if (meta.requestStatus === "fulfilled") {
       navigate("/home");
     }
-  }
-   return (
-     <div className="h-screen flex justify-center back">
-       <div className="text-center mt-14">
-         <h1 className="text-5xl font-semibold mb-4">
-           Twitter
-         </h1>
-         {error !== "" && (
-           <p className="text-red-600 font-meduim max-w-sm">{error}</p>
-         )}
-         {errorMessage !== "" && (
-           <p className="text-red-600 font-medium max-w-sm">{errorMessage}</p>
-         )}
+  };
 
-         <div className="bg-gray-200 p-4 text-left rounded-md mt-2 shadow-lg">
-           <form onSubmit={signupHandler}>
-             <div>
-               <Label htmlFor="name" labelText="Name" id="name-input-title"></Label>
-               <Input
-                 value={name}
+  return (
+    <div className="h-screen flex justify-center back">
+      <div className="text-center mt-14">
+        <h1 className="text-5xl font-semibold mb-4">Nova Socials</h1>
+        {error !== "" && (
+          <p className="text-red-600 font-medium max-w-sm">{error}</p>
+        )}
+        {errorMessage !== "" && (
+          <p className="text-red-600 font-medium max-w-sm">{errorMessage}</p>
+        )}
+        <div className="bg-gray-200 p-4 text-left rounded-md mt-2 shadow-lg">
+          <form onSubmit={signupHandler}>
+            <div>
+              <Label htmlFor="name" labelText="Name" id="name-input-title" />
+              <Input
+                value={name}
                 id="name"
                 placeholder="Name"
                 ariaLabelledBy="name-input-title"
-                callback={setName}/>
-             </div>
-             <div>
+                callback={setName}
+              />
+            </div>
+            <div>
               <Label
                 htmlFor="username"
                 labelText="Username"
@@ -101,8 +98,8 @@ const Signup = () => {
                 value={username}
                 callback={setUsername}
               />
-             </div>
-             <div>
+            </div>
+            <div>
               <Label htmlFor="email" labelText="Email" id="email-input-title" />
               <Input
                 callback={setEmail}
@@ -111,8 +108,8 @@ const Signup = () => {
                 id="email"
                 ariaLabelledBy="email-input-title"
               />
-             </div>
-             <div>
+            </div>
+            <div>
               <Label
                 htmlFor="password"
                 id="password-input-title"
@@ -142,8 +139,8 @@ const Signup = () => {
                   />
                 )}
               </div>
-             </div>
-             <div>
+            </div>
+            <div>
               <Label
                 htmlFor="confirmPassword"
                 id="confirmPassword-input-title"
@@ -156,8 +153,8 @@ const Signup = () => {
                 value={confirmPassword}
                 callback={setConfirmPassword}
               />
-             </div>
-             <div className="flex justify-center">
+            </div>
+            <div className="flex justify-center">
               <button
                 type="submit"
                 className="bg-blue-500 text-white px-3 py-2 rounded-md mt-2 w-36 font-semibold shadow-md"
@@ -165,12 +162,11 @@ const Signup = () => {
                 {!loading ? "Signup" : "Signing In..."}
               </button>
             </div>
-             
-           </form>
-         </div>
-       </div>
-     </div>
-   )
- }
- 
- export default Signup
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default signupHandler;
