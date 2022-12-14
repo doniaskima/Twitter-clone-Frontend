114358b52b95bf099e4b7772ddd20f10fe4e88ac
unknown <doniaskima344@gmail.com>
2022-12-14 12:54:07 +0100

    adding component

---
+++ src/components/MessagesPageComponents/Chats.js
@@ -0,0 +1,11 @@
+import { useEffect } from "react";
+import {useDispatch,useSelctor} from "react-redux";
+import {fetchMessages, newMessage} from "react-router-dom";
+
+const Chats = () => {
+  return (
+    <div>Chats</div>
+  )
+}
+
+export default Chats
