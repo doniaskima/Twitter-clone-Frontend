24b6799fe48f864e2554cca9efc536312d27fca9
unknown <doniaskima344@gmail.com>
2022-12-14 12:57:10 +0100

    nah notything

--- src/components/MessagesPageComponents/Chats.js
+++ src/components/MessagesPageComponents/Chats.js
@@ -1,6 +1,9 @@
 import { useEffect } from "react";
 import {useDispatch,useSelctor} from "react-redux";
-import {fetchMessages, newMessage} from "react-router-dom";
+import {fetchMessages, newMessage} from "../../features/message/messageSlice";
+import Spinner from "./Spinner";
+import Header from "./Header";
+import Message from "./Message";
 
 const Chats = () => {
   return (
