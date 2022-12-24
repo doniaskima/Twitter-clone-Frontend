9c0ae0afbda920e0bf52f0f15cddac974d3baf54
unknown <doniaskima344@gmail.com>
2022-12-24 19:38:16 +0100

    trying to fix profile page

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import SideNavigationBar from "../components/SideNavigationBar/SideNavigationBar";
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className="flex h-screen bg-white">
+    <SideNavigationBar />
+    <div className="w-600 border ml-0 md:ml-28 lg:ml-0">
+      <div className="fixed w-600 h-10 bg-white flex items-center p-2 border ">
+        <span className="font-semibold" >
+          Profile
+        </span>
+      </div>
+      </div>
+    </div>
   )
 }
 
