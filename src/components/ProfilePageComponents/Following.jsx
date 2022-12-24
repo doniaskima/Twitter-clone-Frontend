ad55f9360ca50f7063fa5652057cae5fac3d198c
unknown <doniaskima344@gmail.com>
2022-12-24 20:20:15 +0100

    pff

--- src/components/ProfilePageComponents/Following.jsx
+++ src/components/ProfilePageComponents/Following.jsx
@@ -3,48 +3,41 @@ import { useDispatch, useSelector } from "react-redux";
 import { fetchUserFollowing } from "../../features/user/userSlice";
 import Spinner from "../Spinner";
 
-import { UserTileComponent } from "../";
+import { UserTileComponent } from "../UserTileComponent";
 
 const Following = ({ userId }) => {
-    const {
-        data: { _id: clientId },
-        retrievedUser: { following },
-        profileTabsFetching,
-        errorMessage,
-    } = useSelector((state) => state.user);
+  const {
+    data: { _id: clientId },
+    retrievedUser: { following },
+    profileTabsFetching,
+    errorMessage,
+  } = useSelector((state) => state.user);
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(fetchUserFollowing({ userId, clientId }));
-    }, []);
+  useEffect(() => {
+    dispatch(fetchUserFollowing({ userId, clientId }));
+  }, []);
 
-    return ( <
-        div className = "mt-2" > {
-            following.length != 0 && !profileTabsFetching && errorMessage && ( <
-                div className = "text-center" > { errorMessage } < /div>
-            )
-        } {
-            following.length == 0 && !profileTabsFetching && ( <
-                div className = "text-center" > No Following < /div>
-            )
-        } {
-            profileTabsFetching ? ( <
-                div className = "flex justify-center" >
-                <
-                Spinner / >
-                <
-                /div>
-            ) : (
-                following.map((user) => {
-                    return <UserTileComponent key = { user._id }
-                    user = { user }
-                    />;
-                })
-            )
-        } <
-        /div>
-    );
+  return (
+    <div className="mt-2">
+      {following.length != 0 && !profileTabsFetching && errorMessage && (
+        <div className="text-center">{errorMessage}</div>
+      )}
+      {following.length == 0 && !profileTabsFetching && (
+        <div className="text-center">No Following</div>
+      )}
+      {profileTabsFetching ? (
+        <div className="flex justify-center">
+          <Spinner />
+        </div>
+      ) : (
+        following.map((user) => {
+          return <UserTileComponent key={user._id} user={user} />;
+        })
+      )}
+    </div>
+  );
 };
 
 export default Following;
