1aec2d0b6b79a600c96942abb54f7acdfcde1855
unknown <doniaskima344@gmail.com>
2022-12-26 18:50:26 +0100

    retrievedUser bug

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import SideNavigationBar from "../components/SideNavigationBar/SideNavigationBar";
 import { fetchUserInfo, followUser, unFollowUser } from "../features/user/userSlice";
 import dayjs from "dayjs";
-import {AiOutlineCalendar} from "react-icons/ai"
+import { AiOutlineCalendar } from "react-icons/ai"
 import { useSelector, useDispatch } from "react-redux";
 import { RecentlyJoinedUsers } from '../components/HomePageComponents/RecentlyJoinedUsers';
 import Spinner from "../components/Spinner";
@@ -11,65 +11,68 @@ import { useParams } from 'react-router-dom'
 const Profile = () => {
   const dispatch = useDispatch();
   const { userId } = useParams();
-  const {retrievedUser , retreivedUserLoading:loading}=useSelector(
+  const { retrievedUser, retreivedUserLoading: loading } = useSelector(
     (state) => state.user
   );
   const loggedInUser = useSelector((state) => state.user.data);
   const [showProfileModal, setShowProfileModal] = useState(false);
-  // const isloggedInUser = loggedInUser._is === retrievedUser._id;
+
+  useEffect(() => {
+    dispatch(fetchUserInfo({ userId }));
+  }, [userId]);
 
   return (
     <div className="flex h-screen bg-white">
-    <SideNavigationBar />
-    <div className="w-600 border ml-0 md:ml-28 lg:ml-0">
-      <div className="fixed w-600 h-10 bg-white flex items-center p-2 border ">
-        <span className="font-semibold" >
-          Profile
-        </span>
+      <SideNavigationBar />
+      <div className="w-600 border ml-0 md:ml-28 lg:ml-0">
+        <div className="fixed w-600 h-10 bg-white flex items-center p-2 border ">
+          <span className="font-semibold" >
+            Profile
+          </span>
         </div>
         {
           loading ? (
             <div>
-              <Spinner/>
+              <Spinner />
             </div>
-          ): (
-              <div className='mt-10 w-full'>
-                <div className='p-2 border'>
-                  <img src="https://avatars.githubusercontent.com/u/23558090?v=4" alt="Donia-profile" className="w-28 h-28 mr-2 rounded-full" loading="lazy" />
-                  <div className="flex justify-between items-center">
+          ) : (
+            <div className='mt-10 w-full'>
+              <div className='p-2 border'>
+                <img src="https://avatars.githubusercontent.com/u/23558090?v=4" alt="Donia-profile" className="w-28 h-28 mr-2 rounded-full" loading="lazy" />
+                <div className="flex justify-between items-center">
+                  <div>
+                    <p className="font-semibold text-lg">Donia</p>
+                    <p>@Skima</p>
+                    <button className="py-2 px-3 border-2 border-black rounded-full">
+                      Edit profile
+                    </button>
+                    <div className='py-2 text-base'>bio</div>
                     <div>
-                      <p className="font-semibold text-lg">Donia</p>
-                      <p>@Skima</p>
-                      <button className="py-2 px-3 border-2 border-black rounded-full">
-                        Edit profile
-                      </button>
-                      <div className='py-2 text-base'>bio</div>
-                      <div>
-                        <AiOutlineCalendar />
-                        <p className='text-gray-400'>Joined December 24</p>
-                      </div>
-                      <div className="space-x-2 mt-2">
-                        <span>
-                          <span className="font-semibold">
-                            233
-                          </span>
-                           <span className="text-gray-400"> Following</span>
-                        </span>
-                        <span className="font-semibold">
-                          <span className="font-semibold">
-                            233
-                          </span>
-                           <span className="text-gray-400"> Following</span>
-                        </span>
-                      </div>
-                  
+                      <AiOutlineCalendar />
+                      <p className='text-gray-400'>Joined December 24</p>
                     </div>
-           
+                    <div className="space-x-2 mt-2">
+                      <span>
+                        <span className="font-semibold">
+                          233
+                        </span>
+                        <span className="text-gray-400"> Following</span>
+                      </span>
+                      <span className="font-semibold">
+                        <span className="font-semibold">
+                          233
+                        </span>
+                        <span className="text-gray-400"> Following</span>
+                      </span>
+                    </div>
+
                   </div>
-               
+
                 </div>
-                <SwitchTabComponent userId={userId} />
+
               </div>
+              <SwitchTabComponent userId={userId} />
+            </div>
           )
         }
       </div>
