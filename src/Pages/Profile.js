3fc04c4512e0a8f0c94248d9232965d1f00e8fb5
unknown <doniaskima344@gmail.com>
2022-12-26 19:09:00 +0100

    gonna fix EditProfileModdal , reaaly sick today

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -6,12 +6,13 @@ import { AiOutlineCalendar } from "react-icons/ai"
 import { useSelector, useDispatch } from "react-redux";
 import { RecentlyJoinedUsers } from '../components/HomePageComponents/RecentlyJoinedUsers';
 import Spinner from "../components/Spinner";
+import EditProfileModal from "../components/ProfilePageComponents/EditProfileModal";
 import SwitchTabComponent from "../components/ProfilePageComponents/SwitchTabComponent";
 import { useParams } from 'react-router-dom'
 const Profile = () => {
   const dispatch = useDispatch();
   const { userId } = useParams();
-  const { retreivedUser, retrievedUserLoading: loading } = useSelector(
+  const { retreivedUser, retreivedUserLoading: loading } = useSelector(
     (state) => state.user
   );  
   const loggedInUser = useSelector((state) => state.user.data);
@@ -39,12 +40,17 @@ const Profile = () => {
           ) : (
             <div className='mt-10 w-full'>
               <div className='p-2 border'>
-                <img src="https://avatars.githubusercontent.com/u/23558090?v=4" alt="Donia-profile" className="w-28 h-28 mr-2 rounded-full" loading="lazy" />
+                  <img
+                    src={retreivedUser.profileUrl}
+                    alt={retreivedUser.name}
+                    loading="lazy"
+                    className="w-28 h-28 mr-2 rounded-full"
+                  />
                 <div className="flex justify-between items-center">
                   <div>
                     <p className="font-semibold text-lg">Donia</p>
                     <p>@Skima</p>
-                    <button className="py-2 px-3 border-2 border-black rounded-full">
+                      <button className="py-2 px-3 border-2 border-black rounded-full onClick={() => setShowProfileModal(true)}">
                       Edit profile
                     </button>
                     <div className='py-2 text-base'>bio</div>
@@ -55,15 +61,20 @@ const Profile = () => {
                     <div className="space-x-2 mt-2">
                       <span>
                         <span className="font-semibold">
-                          233
+                            {retreivedUser.following.length}
                         </span>
                         <span className="text-gray-400"> Following</span>
                       </span>
                       <span className="font-semibold">
                         <span className="font-semibold">
-                          233
+                            {retreivedUser.followers.length}
                         </span>
-                        <span className="text-gray-400"> Following</span>
+                          <span className="text-gray-400">
+                            
+                            {retreivedUser.followers.length == 1
+                              ? " Follower"
+                              : " Followers"}
+                        </span>
                       </span>
                     </div>
 
@@ -73,10 +84,19 @@ const Profile = () => {
 
               </div>
               <SwitchTabComponent userId={userId} />
-            </div>
+              </div>
           )
         }
       </div>
+      <div className="ml-8 hidden lg:block">
+        <RecentlyJoinedUsers />
+      </div>
+      {showProfileModal && (
+        <EditProfileModal
+          setShowProfileModal={setShowProfileModal}
+          user={retreivedUser}
+        />
+      )}
     </div>
   )
 }
