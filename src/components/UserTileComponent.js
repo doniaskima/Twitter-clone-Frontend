a441828fe0ab8de782bc0e6c9952449f9f40c262
unknown <doniaskima344@gmail.com>
2022-12-27 20:36:41 +0100

    some fixes

--- src/components/UserTileComponent.js
+++ src/components/UserTileComponent.js
@@ -3,13 +3,16 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import {followUser,unFollowUser}  from "../features/user/userSlice.js"
 
+
 export const UserTileComponent = ({ user }) => {
     const navigate = useNavigate();
-    const [isFollowedByClient, setIsFollowedByClient] = useState(user.isFollowedByClient);
-    const dispatch = useDispatch();
+    const [isFollowedByClient, setIsFollowedByClient] = useState(
+        user.isFollowedByClient
+    );
     const { _id: clientId } = useSelector((state) => state.user.data);
     const isClientTile = user._id === clientId;
-    
+    const dispatch = useDispatch();
+
     const handleClick = () => {
         setIsFollowedByClient((state) => !state);
         if (isFollowedByClient) {
@@ -17,40 +20,39 @@ export const UserTileComponent = ({ user }) => {
         } else {
             dispatch(followUser({ targetId: user._id }));
         }
-    }
+    };
 
-  return (
-      <div
-          key={user._id} 
-          className="flex mb-1 items-start cursor-pointer hover:bg-gray-50 py-2 px-3"
-          onClick={()=>navigate(`/profile/${user._id}`)}
-      >
-          <img
-              src={user.profileUrl}
-              alt={user.name}
-              loading="lazy"
-              className="w-10 h-10 mr-2 rounded-full"
-          />
-          <div className="text-left">
-              <p className="leading-4">{user.name}</p>
-              <p className="leading-4">@{user.username}</p>
-          </div>
-          {isClientTile && (
-              <button
-              onClick={(e) => {
-                e.stopPropagation();
-                handleClick();
-              }}
-              className={
-                isFollowedByClient ? "rounded-full py-2 px-4 text-black font-semibold text-sm ml-auto bg-white border" : "rounded-full py-2 px-4 text-white font-semibold text-sm ml-auto bg-black"
-              }
-              >
-                  {isFollowedByClient ? "Following" : "Follow"}
-              </button>
-          )}
-          
-  </div>
-  )
-}
+    return (
+        <div
+            key={user._id}
+            className="flex mb-1 items-start cursor-pointer hover:bg-gray-50 py-2 px-3"
+            onClick={() => navigate(`/profile/${user._id}`)}
+        >
+            <img
+                src={user.profileUrl}
+                alt={user.name}
+                loading="lazy"
+                className="w-10 h-10 mr-2 rounded-full"
+            />
+            <div className="text-left">
+                <p className="leading-4 font-medium">{user.name}</p>
+                <p className="leading-4 text-gray-400">@{user.username}</p>
+            </div>
+            {!isClientTile && (
+                <button
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        handleClick();
+                    }}
+                    className={
+                        isFollowedByClient ? "rounded-full py-2 px-4 text-black font-semibold text-sm ml-auto bg-white border" : "rounded-full py-2 px-4 text-white font-semibold text-sm ml-auto bg-black"
+                    }
+                >
+                    {isFollowedByClient ? "Following" : "Follow"}
+                </button>
+            )}
+        </div>
+    );
+};
 
 export default UserTileComponent
