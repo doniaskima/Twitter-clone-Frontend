d01073b3d262bcb66fc149c7df655d1446beb1c8
unknown <doniaskima344@gmail.com>
2022-12-27 15:57:29 +0100

    useSearch probem

--- src/Pages/Explore.jsx
+++ src/Pages/Explore.jsx
@@ -13,38 +13,40 @@ const Explore = () => {
     <div className="flex h-screen bg-white">
       <SideNavigationBar />
       <div className="w-600 border ml-0 md:ml-28 lg:ml-0">
-        <span className="font-semibold">
-          Explore
-        </span>
-      </div>
-      <div className="mt-10 w-full px-5">
-        <div className="pt-3">
-          <SearchField
-           value={searchText}
-           callback={(e) => setSearchText(e.target.value)}
-          />
+        <div className="fixed w-600 h-10 bg-white flex items-center p-2 border">
+          <span className="font-semibold" role="heading">
+            Explore
+          </span>
         </div>
-        <div className="w-full pt-4">
-          {loading ? (
-            <div  className="flex justify-center">
-              <Spinner/>
-            </div>
-          ) : (
+        <div className="mt-10 w-full px-5">
+          <div className="pt-3">
+            <SearchField
+              value={searchText}
+              callback={(e) => setSearchText(e.target.value)}
+            />
+          </div>
+          <div className="w-full pt-4">
+            {loading ? (
+              <div className="flex justify-center">
+                <Spinner />
+              </div>
+            ) : (
               <div>
                 {result.length === 0 && (
-                    <p className="text-center font-medium text-gray-500">
-                     Try searching by name or username
-                   </p>
+                  <p className="text-center font-medium text-gray-500">
+                    Try searching by name or username
+                  </p>
                 )}
                 {result.map((user) => (
                   <UserTileComponent user={user} key={user._id} />
                 ))}
               </div>
             )}
+          </div>
         </div>
       </div>
-   </div>
-  )
-}
+    </div>
+  );
+};
 
-export default Explore
+export default Explore;
