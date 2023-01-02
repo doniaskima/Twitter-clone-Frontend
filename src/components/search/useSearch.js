40d8b08ec1efe1fdcb6cad4728da4b755264ee01
unknown <doniaskima344@gmail.com>
2023-01-02 01:09:00 +0100

    really i hate my ife

--- src/components/search/useSearch.js
+++ src/components/search/useSearch.js
@@ -6,6 +6,20 @@ export const useSearch = (searchText) => {
   const [result, setResult] = useState([]);
   const [loading, setLoading] = useState(false);
 
+
+    const getSearchResult = useCallback(async (searchText) => {
+        if (searchText !== "") {
+            setLoading(true);
+            const { data } = await axios.get(
+                `${BaseUrl}/users/search?text=${searchText}`
+            );
+            if (data.success) {
+                setResult(data.users);
+            }
+            setLoading(false);
+        }
+    }, []);
+
   const debouncer = useCallback(function (callback, delay) {
     let timeoutId;
     return function debounced(...args) {
@@ -17,18 +31,6 @@ export const useSearch = (searchText) => {
     };
   }, []);
 
-  const getSearchResult = useCallback(async (searchText) => {
-    if (searchText !== "") {
-      setLoading(true);
-      const { data } = await axios.get(
-        `${BaseUrl}/users/search?text=${searchText}`
-      );
-      if (data.success) {
-        setResult(data.users);
-      }
-      setLoading(false);
-    }
-  }, []);
 
   const debouncedFunction = useCallback(debouncer(getSearchResult, 3000), [
     searchText,
