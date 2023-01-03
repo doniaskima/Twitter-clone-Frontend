cd978aeaa9f36159d99bde4deb21af3543031cfd
unknown <doniaskima344@gmail.com>
2023-01-03 22:13:31 +0100

    gonnaadd essage component

--- src/components/MessagesPageComponents/Message.js
+++ src/components/MessagesPageComponents/Message.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import {useDispatch ,useSelector} from "react-redux";
+import dayjs from "dayjs";
+import { useState } from "react";
 
 const Message = () => {
+  const { _id: id } = useSelector((state) => state.user.data);
+  const timestamp = dayjs(msg.createdAt).format("MMM D, YYYY, h:mm a");
+  const dispatch = useDispatch();
+  
   return (
-    <div>Message</div>
+    <div
+      className="flex w-full mb-4"
+    ></div>
   )
 }
 
