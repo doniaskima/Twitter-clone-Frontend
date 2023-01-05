332f6f763e3977cc14dc54e952904d785f7aaea6
unknown <doniaskima344@gmail.com>
2023-01-05 11:59:18 +0100

    nothing to add still fighting exams

--- src/components/MessagesPageComponents/Message.js
+++ src/components/MessagesPageComponents/Message.js
@@ -2,18 +2,21 @@ import React from 'react'
 import {useDispatch ,useSelector} from "react-redux";
 import dayjs from "dayjs";
 import { useState } from "react";
+import { deleteMessage } from "../../features/message/messageSlice";
 
 const Message = () => {
   const { _id: id } = useSelector((state) => state.user.data);
   const timestamp = dayjs(msg.createdAt).format("MMM D, YYYY, h:mm a");
-  const dispatch = useDispatch();
-  
+  const dispatch = useDispatch();  
+  const [showOptions, setShowOptions] = useState(false);
   return (
     <div
       className="flex w-full mb-4"
+      onMouseEnter={() => setShowOptions(true)}
+      onMouseLeave={() => setShowOptions(false)}
     >
       <div>
-        
+
       </div>
     </div>
   )
