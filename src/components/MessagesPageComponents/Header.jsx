c8294416d1c40dd672146a3b859b7cd22635fa41
unknown <doniaskima344@gmail.com>
2023-01-06 17:17:51 +0100

    just still suffernig from University Exams

--- src/components/MessagesPageComponents/Header.jsx
+++ src/components/MessagesPageComponents/Header.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
 const Header = () => {
+  const navigate = useNavigate();
   return (
-    <div>Header</div>
+  <div className="flex px-2 py-1 items-center relative">
+    Header
+  </div>
   )
 }
 
