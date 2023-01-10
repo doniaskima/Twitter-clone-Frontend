0eefa80d62dda711b6b441d06febbe65ef4ceddd
unknown <doniaskima344@gmail.com>
2023-01-10 23:10:35 +0100

    Fixing Message Component

--- src/components/MessagesPageComponents/Message.js
+++ src/components/MessagesPageComponents/Message.js
@@ -3,7 +3,8 @@ import {useDispatch ,useSelector} from "react-redux";
 import dayjs from "dayjs";
 import { useState } from "react";
 import { deleteMessage } from "../../features/message/messageSlice";
-import { decryptMessage } from '../../utils/BaseUrl';
+import { decryptMessage, copyToClipboard } from '../../utils/BaseUrl';
+import { FaRegCopy } from "react-icons/fa";
 
 const Message = ({msg}) => {
   const { _id: id } = useSelector((state) => state.user.data);
@@ -23,9 +24,22 @@ const Message = ({msg}) => {
         className={`mb-3 whitespace-nowrap py-2 px-3 relative` ${messageByUser} ? "ml-auto bg-blue-400 text-white rounded-br-none rounded-3xl" : "mr-auto rounded-bl-none rounded-3xl bg-slate"}
       >
       {decryptedMessage}
-      <p className="text-black right-0 text-xs">
+      <p className="text-black right-0 text-xs ">
         {timestamp}
       </p>
+      {
+        showOptions && 
+        <div>
+            <i
+              role="button"
+              aria-label="Copy message"
+              className="text-gray-600 text-lg"
+              onClick={() => copyToClipboard(decryptedMessage)}
+            >
+              <FaRegCopy />
+            </i>
+          </div>
+      }
     </div>
     
     </div>
