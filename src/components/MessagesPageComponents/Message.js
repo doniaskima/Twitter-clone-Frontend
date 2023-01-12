2f084c0a9f1c5baf196fcd1daab8795e5a49640e
unknown <doniaskima344@gmail.com>
2023-01-12 20:31:53 +0100

    clean up the code

--- src/components/MessagesPageComponents/Message.js
+++ src/components/MessagesPageComponents/Message.js
@@ -21,17 +21,18 @@ const Message = ({ msg }) => {
       onMouseLeave={() => setShowOptions(false)}
     >
       <div
-        className={`mb-3 whitespace-nowrap py-2 px-3 relative` ${messageByUser} ? "ml-auto bg-blue-400 text-white rounded-br-none rounded-3xl" : "mr-auto rounded-bl-none rounded-3xl bg-slate"}
+        className={`mb-3 whitespace-nowrap py-2 px-3 relative ${messageByUser ? "ml-auto bg-blue-400 text-white rounded-br-none rounded-3xl" : "mr-auto rounded-bl-none rounded-3xl bg-slate"
+          }`}
       >
-      {decryptedMessage}
-      <p className="text-black right-0 text-xs ">
-        {timestamp}
-      </p>
-      {
-        showOptions && 
-        <div
-          aria-label="Message Options"
-          className={`absolute text-black fleex border ${messageByUser} ? "-left-14" : "-right-14`}
+        {decryptedMessage}
+        <p className="absolute text-black -bottom-5 right-0 text-xs">
+          {timestamp}
+        </p>
+        {showOptions && (
+          <div
+            aria-label="Message options"
+            className={`absolute bottom-1 flex border space-x-3 ${messageByUser ? "-left-14" : "-right-14"
+              }`}
           >
             <i
               role="button"
@@ -41,21 +42,22 @@ const Message = ({ msg }) => {
             >
               <FaRegCopy />
             </i>
-
-            <i 
-              aria-label="Delete Message"
+            <i
               role="button"
-              className="text-red-700"
-              onClick={()=> dispatch(deleteMessage({ messageId: msg.messageId }))}
+              aria-label="Delete Message"
+              className="text-red-700 text-lg"
+              onClick={() =>
+                dispatch(deleteMessage({ messageId: msg.messageId }))
+              }
             >
               <FaRegTrashAlt />
             </i>
           </div>
-      }
+        )}
+      </div>
     </div>
-    
-    </div>
-  )
-}
+  );
+};
 
-export default Message
+export default Message;
+ 
