c65cc2d473b0c277b651825e146cae1c491b8c42
unknown <doniaskima344@gmail.com>
2023-01-17 03:49:11 +0100

    gonna change ui for Login and signup pages

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,12 +1,13 @@
-import {useState} from 'react'
+import { useState } from 'react'
 import { Link, Navigate, useNavigate } from "react-router-dom";
 import { Input, Label } from "../components/FormComponents";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import { useSelector, useDispatch } from "react-redux";
 import { loginUserAsync } from "../features/user/userSlice";
+import LogoTwitter from "../assets/logo.png";
 
 const Login = () => {
-  const navigate =useNavigate();
+  const navigate = useNavigate();
   const dispatch = useDispatch();
   const [error, setError] = useState("");
   const [showPassword, setShowPassword] = useState(false);
@@ -27,48 +28,51 @@ const Login = () => {
 
   };
   return (
-    <> 
-  {isUserLoggedIn && <Navigate to="home" replace />}
-    <div className="h-screen flex justify-center">
-      <div className="text-center mt-40">
-        <h1 className="text-5xl font-semibold mb-4"> Twitter clone</h1>
-        {errorMessage !== "" && (
+    <>
+      {isUserLoggedIn && <Navigate to="home" replace />}
+      <div className="h-screen flex justify-center">
+        <div className="text-center mt-40">
+          <div className="flex justify-center">
+            <img className="w-16 mb-4" src={LogoTwitter} alt="LogoTwitter" />
+          </div>
+          <h1 className="text-4xl font-semibold mb-8">Log in to Twitter </h1>
+          {errorMessage !== "" && (
             <p className="text-red-600 font-medium max-w-sm">{errorMessage}</p>
           )}
-        <div className="text-left shadow-lg w-96 p-4 bg-gray-200 rounded-md mt-2">
-          <form onSubmit={loginHandler}>
-            <div className="">
-              <Label htmlFor="email" labelText="Email" id="email-input-title" />
-              <Input
-                callback={setEmail}
-                placeholder="Your Email"
-                value={email}
-                id="email"
-                ariaLabelledBy="email-input-title"
-              />
-            </div>
-            <div>
-              <Label
-                htmlFor="password"
-                labelText="Password"
-                id="password-input-title"
-              />
-              <div className="border-2 border-gray-400 bg-white rounded-md flex items-center">
-                <input
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
-                  placeholder="Your Password"
-                  id="password"
-                  value={password}
-                  type={showPassword ? "text" : "password"}
-                  className="bg-transparent rounded-md w-11/12 p-2 focus:outline-none"
-                  aria-labelledby="password-input-title"
+          <div className="text-left shadow-lg w-96 p-4 bg-gray-200 rounded-md mt-2">
+            <form onSubmit={loginHandler}>
+              <div className="">
+                <Label htmlFor="email" labelText="Email" id="email-input-title" />
+                <Input
+                  callback={setEmail}
+                  placeholder="Your Email"
+                  value={email}
+                  id="email"
+                  ariaLabelledBy="email-input-title"
                 />
-                {showPassword ? (
+              </div>
+              <div>
+                <Label
+                  htmlFor="password"
+                  labelText="Password"
+                  id="password-input-title"
+                />
+                <div className="border-2 border-gray-400 bg-white rounded-md flex items-center">
+                  <input
+                    onChange={(e) => {
+                      setPassword(e.target.value);
+                    }}
+                    placeholder="Your Password"
+                    id="password"
+                    value={password}
+                    type={showPassword ? "text" : "password"}
+                    className="bg-transparent rounded-md w-11/12 p-2 focus:outline-none"
+                    aria-labelledby="password-input-title"
+                  />
+                  {showPassword ? (
                     <AiOutlineEyeInvisible
-                    onClick={() => setShowPassword(false)}
-                    className="text-2xl cursor-pointer"
+                      onClick={() => setShowPassword(false)}
+                      className="text-2xl cursor-pointer"
                     />
                   ) : (
                     <AiOutlineEye
@@ -76,26 +80,26 @@ const Login = () => {
                       className="text-2xl cursor-pointer"
                     />
                   )}
-                  
+
+                </div>
               </div>
+              <div className="flex justify-center mt-2">
+                <button
+                  type="submit"
+                  disabled={email === "" || password === ""}
+                  className="bg-blue-500 text-white px-3 py-2 rounded-md my-2 w-36 font-semibold"
+                >
+                  {!loading ? "Login" : "Logging In..."}
+                </button>
+              </div>
+            </form>
+            <div className="text-center mt-4">
+              <p className="font-normal">
+                Dont have an account?{" "}<Link to="signup">Sign Up!</Link>
+              </p>
             </div>
-            <div className="flex justify-center mt-2">
-              <button
-                type="submit"
-                disabled={email === "" || password === ""}
-                className="bg-blue-500 text-white px-3 py-2 rounded-md my-2 w-36 font-semibold"
-              >
-                {!loading ? "Login" : "Logging In..."}
-              </button>
-            </div>
-          </form>
-          <div className="text-center mt-4">
-            <p className="font-normal">
-              Dont have an account?{" "}<Link to="signup">Sign Up!</Link>
-            </p>
           </div>
         </div>
-      </div>
       </div>
     </>
   );
