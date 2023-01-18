ae5ebe2630b23f85cdff22880a542865f0ba43b6
unknown <doniaskima344@gmail.com>
2023-01-18 19:21:03 +0100

    just centeer the spinner

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -1,13 +1,11 @@
 import Spinner from "./Spinner";
-
 const LoadingPage = () => {
     return (
-        <div className="flex h-screen h-full w-full flex-col justify-center bg-white bg-blend-saturation">
+        <div className="flex flex-col h-screen w-full justify-center items-center bg-blend-saturation bg-white">
             <Spinner />
             <h1 className="mt-4 text-lg font-semibold tracking-wider">Loading...</h1>
         </div>
-    )
-}
-
+    );
+};
 
 export default LoadingPage;
